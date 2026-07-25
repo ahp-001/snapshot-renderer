6bf45050e59a2dfa124ff8a93c8560a33767d887
ahp-001 <ahp.team.official@gmail.com>
2026-07-26 02:06:17 +0500

    Update server.js

--- server.js
+++ server.js
@@ -4,8 +4,24 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+app.use(express.text({
+    type: "*/*",
+    limit: "20mb"
+}));
+
 app.get("/", (req, res) => {
     res.send("Snapshot Renderer is Running 🚀");
+});
+
+app.post("/render", (req, res) => {
+
+    console.log(req.body);
+
+    res.json({
+        success: true,
+        length: req.body.length
+    });
+
 });
 
 app.listen(PORT, () => {
